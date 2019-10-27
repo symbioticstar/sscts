60c6d0a4a87dfe6a6c320dc838cc9facf6a94d23
ss <i@sst.st>
2019-10-27 17:42:43 +0800

    add simple_comparer

--- makefile
+++ makefile
@@ -1,2 +1,5 @@
 all:
 	gcc comparer.c result.c sandbox.c sscts.c -o sscts -O2 -lseccomp -Wall -static
+	gcc simple_comparer.c -o comparer
+install:
+	cp comparer sscts /usr/bin
---
+++ simple_comparer.c
@@ -0,0 +1,53 @@
+#include <ctype.h>
+#include <stdio.h>
+
+#define fgetc fgetc_unlocked
+
+FILE *std, *ans;
+int s, a;
+
+/**
+ * return positive if Accepted
+ * return negative if Wrong
+ * return zero if not finished
+ */
+int next() {
+    while (s == a) {
+        if (!~s) return 1;
+        s = fgetc(std);
+        a = fgetc(ans);
+    }
+    if (!~s)
+        while (isspace(a)) a = fgetc(ans);
+    if (!~a)
+        while (isspace(s)) s = fgetc(std);
+    if (s == a && !~s) return 2;
+    if (isspace(s) && isspace(a)) {
+        while (s != '\n' && isspace(s)) s = fgetc(std);
+        while (a != '\n' && isspace(a)) a = fgetc(std);
+        if (isspace(s)) s = fgetc(std);
+        if (isspace(a)) a = fgetc(ans);
+        return 0;
+    } else {
+        return -1;
+    }
+}
+
+int main(int args, char **argv) {
+    if (args != 3) {
+        puts("0");
+        return 1;
+    }
+    std = fopen(argv[1], "r"), ans = fopen(argv[2], "r");
+    if (!std || !ans) {
+        puts("0");
+        return 2;
+    }
+    s = fgetc(std), a = fgetc(ans);
+    int result = 0;
+    while (!result) {
+        result = next();
+    }
+    printf("%d\n", result);
+    return 0;
+}
