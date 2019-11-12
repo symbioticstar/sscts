b65be098cf75dfd9f945eab30cab0e90b83cf6d1
ss <i@sst.st>
2019-11-12 19:02:38 +0800

    don't throw SCE_CGCU

--- sscts.c
+++ sscts.c
@@ -252,12 +252,8 @@ int main(int argc, char *argv[]) {
             result.user_time /= 1e6;
             result.memory >>= 10;
             result.cpu_time = result.sys_time + result.user_time;
-            if (cleanup_cg(cg_cpu)) {
-                return SCE_CGCU;
-            }
-            if (cleanup_cg(cg_memory)) {
-                return SCE_CGCU;
-            }
+            cleanup_cg(cg_cpu);
+            cleanup_cg(cg_memory);
         } else {
             ssc_result_parse_rusage(&result, &rusage);
             result.cg_enabled = 0;
